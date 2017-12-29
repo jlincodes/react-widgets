a0535e4aa0601aaee97767ba2f6dfa7e61a55662
Julie <julie.lin0812@gmail.com>
2017-12-28 18:22:29 -0800

    Configures webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+let path = require('path');
+
+module.exports = {
+  entry: './index.jsx',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules),
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+};
